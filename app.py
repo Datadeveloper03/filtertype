c4587c00198198ae48d3c2851098e0f8d72409fc
Datadeveloper03 <156871960+Datadeveloper03@users.noreply.github.com>
2025-02-27 22:27:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import joblib
 import plotly.express as px
 
 # Load trained model and preprocessing tools safely
-model_path = "models\filter_recommendation_model.pkl"
-scaler_path = "models\scaler.pkl"
-encoder_path = "models\label_encoder.pkl"
+model_path = "models/filter_recommendation_model.pkl"
+scaler_path = "models/scaler.pkl"
+encoder_path = "models/label_encoder.pkl"
 
 # Try loading the model and scaler
 try:
